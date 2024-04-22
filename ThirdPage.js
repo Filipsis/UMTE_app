c2855f5f4c9cd628600a6e9ec3a8c78d2e63ae1b
Filip Bidlo <bidlofilip@seznam.cz>
2024-04-23 00:44:03 +0200

    rezervace v0.1

--- ThirdPage.js
+++ ThirdPage.js
@@ -2,62 +2,90 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 function ThirdPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loginStatus, setLoginStatus] = useState('');
+    const [bookingId] = useState(''); // Vzorový kód neměl hodnotu, ponecháno prázdné
+    const [mondaydate] = useState('20240422');
+    const [timeslot] = useState('20242804_1330-1400');
+    const [subjectId] = useState('1');
+    const [calendarId] = useState('1');
+    const [src] = useState('weekformaa');
+    const [veri] = useState('0dvZ3zK2dBRYU6UfFz0luiSTOIKN98Qq');
+    const [userName] = useState('Filip Bidlo'); // Predpokládám, že to je statické
+    const [personName0, setPersonName0] = useState('');
+    const [personPass0, setPersonPass0] = useState(true);
+    const [personName1, setPersonName1] = useState('');
+    const [personPass1, setPersonPass1] = useState(false);
+    const [personName2, setPersonName2] = useState('');
+    const [personPass2, setPersonPass2] = useState(false);
+    const [response, setResponse] = useState('');
 
-    const handleLogin = async () => {
-        const bodyContent = `userName=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&submit=++Přihlásit+`;
+    const handleSubmit = async () => {
+        const data = `bookingId=${bookingId}&mondaydate=${mondaydate}&timeslot=${timeslot}&subjectId=${subjectId}&calendarId=${calendarId}&src=${src}&veri=${veri}&userName=${encodeURIComponent(userName)}&personName0=${encodeURIComponent(personName0)}&personPass0=${personPass0}&personName1=${encodeURIComponent(personName1)}&personPass1=${personPass1}&personName2=${encodeURIComponent(personName2)}&personPass2=${personPass2}&submitnew=Objednat`;
 
-        console.log('Odesílám požadavek na:', 'http://www.sokolstezery.cz/ebooking/loginAction');
-        console.log('Data požadavku:', bodyContent);
-
+        console.log('Sending data:', data);
         try {
-            let response = await fetch('http://www.sokolstezery.cz/ebooking/loginAction', {
+            const response = await fetch('http://www.sokolstezery.cz/ebooking/orderAction', {
                 method: 'POST',
+                body: data,
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                    'Connection': 'keep-alive',
-                    'Referer': 'http://www.sokolstezery.cz/ebooking/login',
-                    // Uvedte další hlavičky z logu podle potřeby
-                },
-                body: bodyContent
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
             });
-
-            const textResponse = await response.text();  // Načtení odpovědi jako text
-            console.log('HTTP Status:', response.status);
-            console.log('Odpověď serveru:', textResponse);
-
-            if (response.url.endsWith('index') && response.status === 200) {
-                setLoginStatus('Login úspěšný');
-            } else {
-                setLoginStatus('Přihlášení se nezdařilo');
-                console.log('Nepodařilo se přihlásit, detaily odpovědi:', textResponse);
-            }
+            const text = await response.text();
+            console.log('Response text:', text);
+            setResponse(text);
         } catch (error) {
-            setLoginStatus('Chyba při přihlášení: ' + error.message);
-            console.log('Chyba při přihlášení:', error);
+            console.error('Error sending data:', error);
+            setResponse('Error sending data: ' + error.message);
         }
-    }
+    };
 
     return (
         <View style={styles.container}>
+            <Text style={styles.header}>Zadejte informace o hráčích</Text>
             <TextInput
-                placeholder="Uživatelské jméno"
-                value={username}
-                onChangeText={setUsername}
                 style={styles.input}
+                onChangeText={setPersonName0}
+                value={personName0}
+                placeholder="Jméno hráče 0"
             />
             <TextInput
-                placeholder="Heslo"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
                 style={styles.input}
+                onChangeText={setPersonName1}
+                value={personName1}
+                placeholder="Jméno hráče 1"
             />
-            <Button title="Přihlásit se" onPress={handleLogin} />
-            <Text>{loginStatus}</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setPersonName2}
+                value={personName2}
+                placeholder="Jméno hráče 2"
+            />
+            <View style={styles.checkboxContainer}>
+                <Text>Permanentka hráče 0:</Text>
+                <TextInput
+                    style={styles.checkbox}
+                    onChangeText={() => setPersonPass0(previousState => !previousState)}
+                    value={personPass0.toString()}
+                />
+            </View>
+            <View style={styles.checkboxContainer}>
+                <Text>Permanentka hráče 1:</Text>
+                <TextInput
+                    style={styles.checkbox}
+                    onChangeText={() => setPersonPass1(previousState => !previousState)}
+                    value={personPass1.toString()}
+                />
+            </View>
+            <View style={styles.checkboxContainer}>
+                <Text>Permanentka hráče 2:</Text>
+                <TextInput
+                    style={styles.checkbox}
+                    onChangeText={() => setPersonPass2(previousState => !previousState)}
+                    value={personPass2.toString()}
+                />
+            </View>
+            <Button title="Odeslat" onPress={handleSubmit} />
+            <Text style={styles.response}>{response}</Text>
         </View>
     );
 }
@@ -67,14 +95,30 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20
+    },
+    header: {
+        fontSize: 20,
+        marginBottom: 20
     },
     input: {
-        width: '80%',
-        padding: 10,
-        margin: 10,
+        height: 40,
+        marginVertical: 12,
         borderWidth: 1,
-        borderColor: '#ccc',
+        padding: 10,
+        width: '100%'
     },
+    checkboxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    checkbox: {
+        margin: 8
+    },
+    response: {
+        marginTop: 20,
+        color: 'green'
+    }
 });
 
 export default ThirdPage;
