1630e89e4f59686bfc0c374ab7e460e6cbb12d04
Filip Bidlo <bidlofilip@seznam.cz>
2024-04-28 13:00:23 +0200

    Teniscentrum DTJ

--- TennisCourts.js
+++ TennisCourts.js
@@ -1,67 +1,91 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { ScrollView, StyleSheet, SafeAreaView, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const TennisCourts = () => {
     const customScript1 = `document.body.innerHTML = document.getElementById('calendarTable').outerHTML;true;`;
 
-    // Immediately attempt to click the button when the script runs, without waiting for DOMContentLoaded
     const customScript2 = `
+        var buttonClicked = false;
+
         function handleButtonClick() {
-            const button = document.getElementById('BTN1');
-            if (button) {
-                button.click();
-                const table = document.getElementById('TAB_ROZPIS_TabPanel_l_1_Table_lekce');
-                if (table) {
-                    const style = document.createElement('style');
-                    style.textContent = \`
-                        .ntabulka, .ntabulka th, .ntabulka td {
-                            font-size: 12px;
-                            padding: 4px;
-                        }
-                        .ntabulka {
-                            min-width: 1200px; // Ensures the table is scrollable horizontally
-                            table-layout: fixed; // Keeps the column widths consistent
-                        }
-                        body {
-                            overflow-x: scroll; // Allows scrolling on x-axis if needed
-                        }
-                    \`;
-                    document.head.appendChild(style);
-                    document.body.innerHTML = table.outerHTML;
+            if (!buttonClicked) {
+                const button = document.getElementById('BTN1');
+                if (button) {
+                    button.click();
+                    buttonClicked = true;
+                    const table = document.getElementById('TAB_ROZPIS_TabPanel_l_1_Table_lekce');
+                    if (table) {
+                        const style = document.createElement('style');
+                        style.textContent = \`
+                            .ntabulka, .ntabulka th, .ntabulka td {
+                                font-size: 12px;
+                                padding: 4px;
+                            }
+                            .ntabulka {
+                                min-width: 800px;
+                                table-layout: fixed;
+                            }
+                            body {
+                                overflow-x: scroll;
+                            }
+                        \`;
+                        document.head.appendChild(style);
+                        document.body.innerHTML = table.outerHTML;
+                    }
+                } else {
+                    setTimeout(handleButtonClick, 100);
                 }
-            } else {
-                // If the button isn't available yet, try again shortly
-                setTimeout(handleButtonClick, 100);
             }
         }
         
         handleButtonClick();
         true;
     `;
+    const customScript3 = `
+        document.body.innerHTML = document.querySelector('.p-3.card-body').outerHTML;
+        window.ReactNativeWebView.postMessage(document.body.innerHTML);
+        true;
+    `;
 
     return (
         <SafeAreaView style={styles.container}>
-            <WebView
-                source={{ uri: 'http://www.sokolstezery.cz/ebooking/weekformaa?calendarId=1' }}
-                injectedJavaScript={customScript1}
-                injectedJavaScriptForMainFrameOnly={false}
-                style={styles.webview}
-                onLoad={() => console.log('WebView 1 loaded!')}
-                onMessage={(event) => {
-                    console.log('event 1: ', event)
-                }}
-            />
-            <WebView
-                source={{ uri: 'https://memberzone.cz/sportparkhit/' }}
-                injectedJavaScript={customScript2}
-                injectedJavaScriptForMainFrameOnly={false}
-                style={styles.webview}
-                onLoad={() => console.log('WebView 2 loaded!')}
-                onMessage={(event) => {
-                    console.log('event 2: ', event)
-                }}
-            />
+            <ScrollView style={styles.scrollView}>
+                <Text style={styles.header}>Stěžery</Text>
+                <WebView
+                    source={{ uri: 'http://www.sokolstezery.cz/ebooking/weekformaa?calendarId=1' }}
+                    injectedJavaScript={customScript1}
+                    injectedJavaScriptForMainFrameOnly={false}
+                    style={styles.webviewSmall}
+                    onLoad={() => console.log('WebView 1 loaded!')}
+                    onMessage={(event) => {
+                        console.log('event 1: ', event)
+                    }}
+                />
+                <Text style={styles.header}>Sportpark HIT</Text>
+                <WebView
+                    source={{ uri: 'https://memberzone.cz/sportparkhit/' }}
+                    injectedJavaScript={customScript2}
+                    injectedJavaScriptForMainFrameOnly={false}
+                    style={styles.webviewLarge}
+                  //  onLoad={() => console.log('WebView 2 loaded!')}
+                    onMessage={(event) => {
+                        console.log('event 2: ', event)
+                    }}
+                />
+                <View style={{ height: 60 }} />
+                <Text style={styles.header}>Teniscentrum DTJ</Text>
+                <WebView
+                    source={{ uri: 'https://www.rezzy.eu/tenant-6/schedules' }}
+                    injectedJavaScript={customScript3}
+                    injectedJavaScriptForMainFrameOnly={false}
+                    style={styles.webviewSmall}
+                    onLoadEnd={() => console.log('WebView 3 loaded!')}
+                    onMessage={(event) => {
+                        console.log('WebView 3 content: ', event.nativeEvent.data)
+                    }}
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -71,9 +95,20 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: 20,
     },
-    webview: {
+    scrollView: {
         flex: 1,
-    }
+    },
+    webviewSmall: {
+        height: 300,  // Fixed height for specific adjustment
+    },
+    webviewLarge: {
+        height: 300,  // Fixed height for specific adjustment
+    },
+    header: {
+        fontSize: 20,
+        marginBottom: 10,
+        textAlign: 'center',
+    },
 });
 
 export default TennisCourts;
