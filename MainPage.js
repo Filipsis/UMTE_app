e6adf42e63519d9934429b441e91d99c527cb6d1
Filip Bidlo <bidlofilip@seznam.cz>
2024-05-05 20:57:42 +0200

    Porizeni fotky

--- MainPage.js
+++ MainPage.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, ActivityIndicator } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
     const [address, setAddress] = useState('');
     const [isLoading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    const [openCamera, setOpenCamera] = useState(false);
+    const cameraRef = useRef(null);
+    const [pictureTaken, setPictureTaken] = useState(false);
 
     useEffect(() => {
         (async () => {
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
                 setError('Přístup k poloze zamítnut, pouze omezené funkce aplikace.');
+                return;
+            }
+
+            status = await Camera.requestCameraPermissionsAsync();
+            if (status.status !== 'granted') {
+                setError('Přístup ke kameře zamítnut, pouze omezené funkce aplikace.');
                 return;
             }
         })();
@@ -37,11 +48,61 @@ export default function App() {
         }
     };
 
+
+    const takePicture = async () => {
+        if (cameraRef.current && !pictureTaken) {
+            setPictureTaken(true);  // Set flag to prevent multiple invocations
+            const options = { quality: 0.5, base64: true };
+            try {
+                const data = await cameraRef.current.takePictureAsync(options);
+                const asset = await MediaLibrary.createAssetAsync(data.uri);
+            } catch (error) {
+                console.log('Chyba při pořizování fotky:', error);
+            } finally {
+                if (cameraRef.current) {
+                    setOpenCamera(false);
+                } else {
+                    console.log('Kamera není k dispozici');
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        // Reset the pictureTaken flag when openCamera changes
+        setPictureTaken(false);
+    }, [openCamera]);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await MediaLibrary.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setError('Přístup k médiím zamítnut, pouze omezené funkce aplikace.');
+                return;
+            }
+        })();
+    }, []);
+
+    if (openCamera) {
+        return (
+            <Camera
+                style={{ flex: 1 }}
+                type={Camera.Constants.Type.front}
+                ref={cameraRef}
+                onCameraReady={takePicture}
+            />
+        );
+    }
+
     return (
         <View style={styles.container}>
             <Button
                 title="Zjisti moji adresu"
                 onPress={fetchAddressInfo}
+            />
+            <Button
+                title="Pořiď foto"
+                onPress={() => setOpenCamera(true)}
             />
             {isLoading ? (
                 <ActivityIndicator size="large" color="#0000ff" />
